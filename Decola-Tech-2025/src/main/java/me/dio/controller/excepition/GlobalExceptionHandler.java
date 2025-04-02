4283175b269d44c726d24948cf89ea1e769fb2be
Isaac <isaacluiz0808@gmail.com>
2025-04-02 20:53:05 -0300

    Adicionando tratamento de exceções

--- Decola-Tech-2025/src/main/java/me/dio/controller/excepition/GlobalExceptionHandler.java
+++ Decola-Tech-2025/src/main/java/me/dio/controller/excepition/GlobalExceptionHandler.java
@@ -1,8 +1,36 @@
 package me.dio.controller.excepition;
 
+import java.util.NoSuchElementException;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
-public class GlobalExceptionHandler {
+import me.dio.model.User;
+import me.dio.service.UserService;
 
+@RestControllerAdvice	
+public class GlobalExceptionHandler {
+	private final Logger logge = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<String> handleBussinesException(IllegalArgumentException bussinesException){
+		return new ResponseEntity<>(bussinesException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+	}
+	
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException){
+		return new ResponseEntity<>("Resource ID not found", HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(Throwable.class)
+	public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException){
+		var message = "Unexpected server error, see the logs";
+		logge.error(message, unexpectedException);
+		return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
